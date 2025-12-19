11b0a2b337f3b3a0c85244c605b9a11e2d216533
Nihal Morshed <nilumorshed@gmail.com>
2025-12-19 16:31:03 +0600

    added movies grid with movies

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -10,6 +10,19 @@ export default function MoviesGrid(){
     }, []);
     return(
         <div className="movies-grid">
+            {
+                movies.map(movie => (
+                    <div key={movie.id} className="movie-card">
+                        <img src={`images/${movie.image}`} alt={movie.title}/>
+                        <div className="movie-card-info">
+                            <h3 className="movie-card-title">{movie.title}</h3>
+                            <p className="movie-card-genre">{movie.genre}</p>
+                            <p className="movie-card-rating">{movie.rating}</p>
+                        </div>
+                    </div>
+
+                ))
+            }
         
         </div>
 
