66c0aa7a085f5518c40aaf3a5eee3503c3d2a27c
Nihal Morshed <nilumorshed@gmail.com>
2025-12-20 01:31:07 +0600

    added image error handler

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,30 +1,26 @@
 import React, {useState, useEffect} from "react";
 import '../styles.css'
+import MovieCard from "./MovieCard";
 
 export default function MoviesGrid(){
     const [movies, setMovies] = useState([]);
-    
-    useEffect(()=>{
-        fetch("movies.json").then(response => response.json()).then(data => setMovies(data))
 
-    }, []);
+    useEffect(()=>{
+        fetch("movies.json").then(resp => resp.json()).then(data => setMovies(data));
+
+    },[]);
+
     return(
         <div className="movies-grid">
             {
-                movies.map(movie => (
-                    <div key={movie.id} className="movie-card">
-                        <img src={`images/${movie.image}`} alt={movie.title}/>
-                        <div className="movie-card-info">
-                            <h3 className="movie-card-title">{movie.title}</h3>
-                            <p className="movie-card-genre">{movie.genre}</p>
-                            <p className="movie-card-rating">{movie.rating}</p>
-                        </div>
-                    </div>
-
-                ))
+                movies.map(
+                    movie => (
+                        <MovieCard movie={movie}></MovieCard>
+                            
+                        
+                    )
+                )
             }
-        
         </div>
-
     );
 }
