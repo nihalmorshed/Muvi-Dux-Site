e4c7fb98fc456f96de97624ae14fc71d2743db53
Nihal Morshed <nilumorshed@gmail.com>
2025-12-21 21:01:08 +0600

    added search bar

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,26 +1,29 @@
-import React, {useState, useEffect} from "react";
-import '../styles.css'
+import React, { useState, useEffect } from "react";
+import "../styles.css";
 import MovieCard from "./MovieCard";
 
-export default function MoviesGrid(){
-    const [movies, setMovies] = useState([]);
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(()=>{
-        fetch("movies.json").then(resp => resp.json()).then(data => setMovies(data));
+  useEffect(() => {
+    fetch("movies.json")
+      .then((resp) => resp.json())
+      .then((data) => setMovies(data));
+  }, []);
 
-    },[]);
-
-    return(
-        <div className="movies-grid">
-            {
-                movies.map(
-                    movie => (
-                        <MovieCard movie={movie}></MovieCard>
-                            
-                        
-                    )
-                )
-            }
-        </div>
-    );
+  return (
+    <div className="">
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Enter Movie Name...."
+      />
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
 }
